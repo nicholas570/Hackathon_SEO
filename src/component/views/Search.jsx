777cdbc1420133f7c6dbd03f3a1c4529c39c1df6
nicolas <nicolas.mercier570@gmail.com>
2021-01-13 21:52:53 +0100

    Added meta in views

--- src/component/views/Search.jsx
+++ src/component/views/Search.jsx
@@ -7,6 +7,11 @@ function Search() {
       <Helmet>
         <meta charSet="utf-8" />
         <meta name="description" content="La page de recherche" />
+        <meta name="author" content="Les Stagiaires X Paye Ton Stage" />
+        <meta
+          name="keywords"
+          content="stage, alternance, dev, developpeur, tech, etudes"
+        />
         <title>Recherche</title>
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
