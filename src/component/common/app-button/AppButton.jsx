f83e8da857a205db956ce98e61af4597222c64d8
baptiste_gfy <geffroy.baptiste@outlook.fr>
2021-01-14 03:26:06 +0100

    create a component button for all the app

---
+++ src/component/common/app-button/AppButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from '../../../css/AppButton.module.css';
+
+function AppButton(props) {
+  const { title, srcImg, altImg /* isSubmit, disabled, onClick */ } = props;
+  return (
+    <button
+      className={styles.appButton}
+      // className={`${styles.appButton} ${disabled && styles.btnDisabled}`}
+      type="button"
+      // type={isSubmit ? 'submit' : 'button'}
+      // disabled={disabled}
+      // onClick={() => onClick()}
+    >
+      {srcImg && <img className={styles.btnIcon} src={srcImg} alt={altImg} />}
+      {title}
+    </button>
+  );
+}
+
+AppButton.defaultProps = {
+  srcImg: null,
+  altImg: null,
+  // isSubmit: false,
+  // disabled: false,
+};
+
+AppButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  srcImg: PropTypes.string,
+  altImg: PropTypes.string,
+  // isSubmit: PropTypes.bool,
+  // disabled: PropTypes.bool,
+  // onClick: PropTypes.func.isRequired,
+};
+
+export default AppButton;
