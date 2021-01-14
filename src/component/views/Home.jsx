2431e5a2e7a5f53ac7d13e314b5dafb94e37a324
baptiste_gfy <geffroy.baptiste@outlook.fr>
2021-01-14 03:27:31 +0100

    create the home page

--- src/component/views/Home.jsx
+++ src/component/views/Home.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
+import Styles from '../../css/Home.module.css';
+import AppButton from '../common/app-button/AppButton';
+
 function Home() {
   return (
-    <div>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta name="description" content="La home page" />
-        <meta name="author" content="Les Stagiaires X Paye Ton Stage" />
-        <meta
-          name="keywords"
-          content="stage, alternance, dev, developpeur, tech, etudes"
-        />
-        <title>Inscription | Connection</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-      </Helmet>
-      La home
-    </div>
+    <section className={Styles.home}>
+      <div className={Styles.homeBgImg}>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <meta name="description" content="La home page" />
+          <meta name="author" content="Les Stagiaires X Paye Ton Stage" />
+          <meta
+            name="keywords"
+            content="stage, alternance, dev, developpeur, tech, etudes"
+          />
+          <title>Inscription | Connection</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+
+        <div className={Styles.homeContent}>
+          <h1 className={Styles.homeTitle}>Paye ton stage</h1>
+          <p className={Styles.homeDescription}>Lorem lorem lorem</p>
+          <AppButton title="Inscription stagiaire" />
+          <AppButton title="Inscription entreprise" />
+          <AppButton title="Connexion" />
+        </div>
+      </div>
+    </section>
   );
 }
 
