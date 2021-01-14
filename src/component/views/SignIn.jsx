f5c8ff506d1f4408ef5b7855e343585db1c606b1
baptiste_gfy <geffroy.baptiste@outlook.fr>
2021-01-14 09:49:44 +0100

    modify component and css signIn

--- src/component/views/SignIn.jsx
+++ src/component/views/SignIn.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Styles from '../../css/SignIn.module.css';
 import AppButton from '../common/app-button/AppButton';
+import AppInput from '../common/app-input/AppInput';
 
 const SignIn = () => {
   return (
@@ -13,8 +14,14 @@ const SignIn = () => {
             <p className={Styles.signInDescription}>Lorem lorem lorem</p>
           </div>
           <form className={Styles.signInForm}>
-            <h2>Connexion</h2>
+            <h2 className={Styles.signInSecondTitle}>Connexion</h2>
+            <br />
+            <AppInput label="adresse email :" placeholder="email@email.com" />
+            <AppInput label="mot de passe :" placeholder="********" />
+            <p className={Styles.signInForgot}>mot de passe oublié ?</p>
+            <br />
             <AppButton title="Je me connecte" />
+            <br />
           </form>
         </div>
       </div>
