326f87c15a4b341fd509e8417b4835720380b18b
Florian Milcendeau <fmilcendeau85@gmail.com>
2021-01-14 14:22:16 +0100

    init

--- src/component/payement/AnnoucementCard/AnnoucementCard.jsx
+++ src/component/payement/AnnoucementCard/AnnoucementCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import style from './AnnoucementCard.module.css';
 import announcementPropType from '../../prop-types/AnnouncementPropTypes';
